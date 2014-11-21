e812a07a52d209a195303351a0ca2cbc3a153cfb
odnalor <rolandoespina3@gmail.com>
2014-11-21 00:42:59 -0300

    Se crea pantalla splash , se debe crear logo de aplicacion para remplazar

---
+++ app/src/main/java/com/ubicapet/rolando/ubicapet_1/SplashScreenActivity.java
@@ -0,0 +1,61 @@
+package com.ubicapet.rolando.ubicapet_1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class SplashScreenActivity extends Activity {
+
+    private static final long SPLASH_SCREEN_DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent().setClass(SplashScreenActivity.this, MyActivity.class);
+                startActivity(intent);
+
+
+                finish();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
